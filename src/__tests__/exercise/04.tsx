783d87dba0d97b3abd513ceece8a3589026c0225
Matjah Sonneveld <matjah.sonneveld@blueconic.com>
2022-05-08 13:45:10 +0200

    feat: complete exercise 04 extra 1

--- src/__tests__/exercise/04.tsx
+++ src/__tests__/exercise/04.tsx
@@ -6,30 +6,26 @@ import userEvent from "@testing-library/user-event";
 import Login from "../../components/login";
 
 test("submitting the form calls onSubmit with username and password", async () => {
-  let submittedData: Record<string, unknown> = {};
-
-  const handleSubmit = (data: Record<string, unknown>) =>
-    (submittedData = data);
+  const handleSubmit = jest.fn();
 
   render(<Login onSubmit={handleSubmit} />);
 
   const username = await screen.getByLabelText(/username/i);
   const password = await screen.getByLabelText(/password/i);
+  const submit = await screen.getByRole("button", {name: /submit/i});
 
   await userEvent.type(username, "someUsername");
   await userEvent.type(password, "somePassword");
-
-  const submit = await screen.getByRole("button", {name: /submit/i});
   await userEvent.click(submit);
 
-  expect(submittedData).toEqual({
+  expect(handleSubmit).toHaveBeencalledWith({
     username: "someUsername",
     password: "somePassword",
   });
 });
 
 /*
-eslint
-  no-unused-vars: "off",
-  @typescript-eslint/no-unused-vars: "off",
-*/
+ eslint
+ no-unused-vars: "off",
+ @typescript-eslint/no-unused-vars: "off",
+ */
