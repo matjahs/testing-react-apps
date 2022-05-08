6f2b879488808c6a0ae92f960f33764a6bf25dcc
Matjah Sonneveld <matjah.sonneveld@blueconic.com>
2022-05-08 12:51:15 +0200

    feat: complete exercise 03

--- src/__tests__/exercise/03.tsx
+++ src/__tests__/exercise/03.tsx
@@ -1,34 +1,24 @@
 // Avoid implementation details
 // http://localhost:3000/counter
 
-// 🐨 add `screen` to the import here:
-import {render, fireEvent} from "@testing-library/react";
+import {render, screen} from "@testing-library/react";
+import userEvent from "@testing-library/user-event";
 import Counter from "../../components/counter";
 
-test("counter increments and decrements when the buttons are clicked", () => {
-  const {container} = render(<Counter />);
-  // 🐨 replace these with screen queries
-  // 💰 you can use `getByText` for each of these (`getByRole` can work for the button too)
-  const [decrement, increment] = Array.from(
-    container.querySelectorAll("button"),
-  );
+test("counter increments and decrements when the buttons are clicked", async () => {
+  render(<Counter />);
 
-  // 🦺 you'll notice that React Testing Library queries do null-checks for you
-  // automatically, so you can ditch all this null-checking stuff.
-  if (!decrement || !increment) {
-    throw new Error("decrement and increment not found");
-  }
-  if (!(container.firstChild instanceof HTMLElement)) {
-    throw new Error("first child is not a div");
-  }
+  const decrement = screen.getByRole("button", {name: /decrement/i});
+  const increment = screen.getByRole("button", {name: /increment/i});
+  const message = screen.getByText(/current count/i);
 
-  const message = container.firstChild.querySelector("div");
-  if (!message) {
-    throw new Error(`couldn't find message div`);
-  }
   expect(message).toHaveTextContent("Current count: 0");
-  fireEvent.click(increment);
-  expect(message).toHaveTextContent("Current count: 1");
-  fireEvent.click(decrement);
-  expect(message).toHaveTextContent("Current count: 0");
+  await userEvent.click(increment);
+
+  const message2 = screen.getByText(/current count/i);
+  expect(message2).toHaveTextContent("Current count: 1");
+
+  await userEvent.click(decrement);
+  const message3 = screen.getByText(/current count/i);
+  expect(message3).toHaveTextContent("Current count: 0");
 });
