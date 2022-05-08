46aa03483439c420cddf160b086dddd084e0d6c4
Matjah Sonneveld <matjah.sonneveld@blueconic.com>
2022-05-08 11:17:14 +0200

    refactor: update prettier config

--- src/__tests__/exercise/01.tsx
+++ src/__tests__/exercise/01.tsx
@@ -1,29 +1,29 @@
 // simple test with ReactDOM
 // http://localhost:3000/counter
 
-import * as ReactDOM from 'react-dom'
-import Counter from '../../components/counter'
-import {act} from 'react-dom/test-utils'
+import * as ReactDOM from "react-dom";
+import Counter from "../../components/counter";
+import {act} from "react-dom/test-utils";
 
-test('counter increments and decrements when the buttons are clicked', () => {
-  const div = document.createElement('div')
-  document.body.append(div)
+test("counter increments and decrements when the buttons are clicked", () => {
+  const div = document.createElement("div");
+  document.body.append(div);
 
-  ReactDOM.render(<Counter />, div)
+  ReactDOM.render(<Counter />, div);
 
-  const buttons = div.querySelectorAll('button')
-  const decrement = buttons.item(0)
-  const increment = buttons.item(1)
-  const message = div.firstElementChild.querySelector('div')
+  const buttons = div.querySelectorAll("button");
+  const decrement = buttons.item(0);
+  const increment = buttons.item(1);
+  const message = div.firstElementChild.querySelector("div");
 
-  expect(message).toHaveTextContent('Current count: 0')
-  act(() => increment.click())
-  expect(message).toHaveTextContent('Current count: 1')
-  act(() => decrement.click())
-  expect(message).toHaveTextContent('Current count: 0')
+  expect(message).toHaveTextContent("Current count: 0");
+  act(() => increment.click());
+  expect(message).toHaveTextContent("Current count: 1");
+  act(() => decrement.click());
+  expect(message).toHaveTextContent("Current count: 0");
 
-  div.remove()
-})
+  div.remove();
+});
 
 /*
 eslint
